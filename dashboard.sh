960199594dbeb4fc5dce65500a72559b3765d3d2
Emmanuel Nyachoke <achachiez@gmail.com>
2024-10-23 21:15:14 +0300

    Update compose

--- dashboard.sh
+++ dashboard.sh
@@ -7,34 +7,34 @@ then
       exit 1
 fi
 
-docker-compose exec mon1 ceph mgr module enable dashboard
-docker-compose exec mon1 ceph dashboard create-self-signed-cert
-docker-compose exec mon1 ceph mgr module disable dashboard
-docker-compose exec mon1 ceph mgr module enable dashboard
-docker-compose exec mon1 ceph config set mgr mgr/dashboard/server_addr mgr
-docker-compose exec mon1 ceph config set mgr mgr/dashboard/server_port 8443
-docker-compose exec mon1 ceph mgr services
-docker-compose exec mon1 ceph dashboard set-login-credentials admin 123456
-docker-compose exec mon1 radosgw-admin user create --uid=1000 --display-name=user --system
-docker-compose exec mon1 radosgw-admin user info --uid=1000
-access_key=$(docker-compose exec mon1 radosgw-admin user info --uid=1000 | grep access_key | awk '{print $2}' | sed 's/"//g'|sed 's/,//g')
-secret_key=$(docker-compose exec mon1 radosgw-admin user info --uid=1000 | grep secret_key | awk '{print $2}' | sed 's/"//g'|sed 's/,//g')
+docker compose exec mon1 ceph mgr module enable dashboard
+docker compose exec mon1 ceph dashboard create-self-signed-cert
+docker compose exec mon1 ceph mgr module disable dashboard
+docker compose exec mon1 ceph mgr module enable dashboard
+docker compose exec mon1 ceph config set mgr mgr/dashboard/server_addr mgr
+docker compose exec mon1 ceph config set mgr mgr/dashboard/server_port 8443
+docker compose exec mon1 ceph mgr services
+docker compose exec mon1 ceph dashboard set-login-credentials admin 123456
+docker compose exec mon1 radosgw-admin user create --uid=1000 --display-name=user --system
+docker compose exec mon1 radosgw-admin user info --uid=1000
+access_key=$(docker compose exec mon1 radosgw-admin user info --uid=1000 | grep access_key | awk '{print $2}' | sed 's/"//g'|sed 's/,//g')
+secret_key=$(docker compose exec mon1 radosgw-admin user info --uid=1000 | grep secret_key | awk '{print $2}' | sed 's/"//g'|sed 's/,//g')
 echo $access_key
 echo $secret_key
 
 
-docker-compose exec mon1 ceph dashboard set-rgw-api-user-id 1000
+docker compose exec mon1 ceph dashboard set-rgw-api-user-id 1000
 
-docker-compose exec mon1 ceph dashboard set-rgw-api-port 8080
-docker-compose exec mon1 ceph dashboard set-rgw-api-host $S3_HOST
-docker-compose exec mon1 ceph dashboard set-rgw-api-scheme http
-docker-compose exec mon1 ceph dashboard set-rgw-api-access-key $access_key
-docker-compose exec mon1 ceph dashboard set-rgw-api-secret-key $secret_key
-docker-compose exec mon1 ceph dashboard set-rgw-api-ssl-verify False
+docker compose exec mon1 ceph dashboard set-rgw-api-port 8080
+docker compose exec mon1 ceph dashboard set-rgw-api-host $S3_HOST
+docker compose exec mon1 ceph dashboard set-rgw-api-scheme http
+docker compose exec mon1 ceph dashboard set-rgw-api-access-key $access_key
+docker compose exec mon1 ceph dashboard set-rgw-api-secret-key $secret_key
+docker compose exec mon1 ceph dashboard set-rgw-api-ssl-verify False
 
-docker-compose exec mon1 ceph dashboard get-rgw-api-port
-docker-compose exec mon1 ceph dashboard get-rgw-api-host
-docker-compose exec mon1 ceph dashboard get-rgw-api-access-key
-docker-compose exec mon1 ceph dashboard get-rgw-api-secret-key
-docker-compose exec mon1 ceph dashboard get-rgw-api-scheme
-docker-compose exec mon1 ceph dashboard get-rgw-api-ssl-verify
+docker compose exec mon1 ceph dashboard get-rgw-api-port
+docker compose exec mon1 ceph dashboard get-rgw-api-host
+docker compose exec mon1 ceph dashboard get-rgw-api-access-key
+docker compose exec mon1 ceph dashboard get-rgw-api-secret-key
+docker compose exec mon1 ceph dashboard get-rgw-api-scheme
+docker compose exec mon1 ceph dashboard get-rgw-api-ssl-verify
--- start.sh
+++ start.sh
@@ -2,22 +2,22 @@
 set -x
 source .env 
 docker network create --driver bridge --subnet=${MON1_CEPH_PUBLIC_NETWORK} ceph-cluster-net
-docker-compose up -d mon1 mgr
+docker compose up -d mon1 mgr
 sudo chmod 777 ceph/ceph/ceph.conf
 sudo cat conf >> ceph/ceph/ceph.conf
-docker-compose restart mon1 mgr
-docker-compose exec mon1 chown -R ceph:ceph /etc/ceph
-docker-compose exec mon1 chown -R ceph:ceph /var/lib/ceph
-docker-compose exec mon1 ceph osd pool create cephfs_data 128
-docker-compose exec mon1 ceph auth get client.bootstrap-rgw -o /var/lib/ceph/bootstrap-rgw/ceph.keyring
-docker-compose exec mon1 ceph auth get client.bootstrap-osd -o /var/lib/ceph/bootstrap-osd/ceph.keyring
-docker-compose exec mon1 ceph auth get client.bootstrap-mds -o /var/lib/ceph/bootstrap-mds/ceph.keyring
+docker compose restart mon1 mgr
+docker compose exec mon1 chown -R ceph:ceph /etc/ceph
+docker compose exec mon1 chown -R ceph:ceph /var/lib/ceph
+docker compose exec mon1 ceph osd pool create cephfs_data 128
+docker compose exec mon1 ceph auth get client.bootstrap-rgw -o /var/lib/ceph/bootstrap-rgw/ceph.keyring
+docker compose exec mon1 ceph auth get client.bootstrap-osd -o /var/lib/ceph/bootstrap-osd/ceph.keyring
+docker compose exec mon1 ceph auth get client.bootstrap-mds -o /var/lib/ceph/bootstrap-mds/ceph.keyring
 
 
-docker-compose up -d osd1 osd2 osd3 osd4
+docker compose up -d osd1 osd2 osd3 osd4
 
-docker-compose up -d rgw1
-docker-compose up -d mds1
+docker compose up -d rgw1
+docker compose up -d mds1
 sleep 3
-docker-compose ps -a
-# docker-compose logs -f osd1
+docker compose ps -a
+# docker compose logs -f osd1
